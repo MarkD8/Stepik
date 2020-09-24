21422c2f9f911cccea995d675b58b0ec9f146ac2
MarkDragunov <xjK-mG5-pZA-zhU>
2020-09-24 21:09:00 +0300

    Add homework from 4 module

---
+++ week 4/conftest.py
@@ -0,0 +1,25 @@
+from selenium.webdriver.chrome.options import Options
+import pytest
+from selenium import webdriver
+
+# @pytest.mark.parametrize('language', ["ru", "es", "fr"])
+# def test_language_should_change(browser, language):
+#     link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
+#     browser.get(link)
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='ru',
+                     help="Choose language")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
+    browser.maximize_window()
+
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
---
+++ week 4/test_items.py
@@ -0,0 +1,51 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import pytest
+
+
+@pytest.mark.test1
+def test_is_button_add_to_basket_present_on_page_coder_at_work(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+
+    time.sleep(30)
+
+    # Ищем на странице кнопку добавления товара в корзину
+    try:
+        button = WebDriverWait(browser, 5).until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, ".btn-add-to-basket")))
+        found = True
+
+    # Если кнопку не нашли (возникла ошибка), то выполняем этот кусок кода
+    except:
+        found = False
+
+    assert found == True, "Button is not found"
+
+
+@pytest.mark.test2
+def test_is_button_add_to_basket_present_on_page_hacking_exposed_wireless(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/hacking-exposed-wireless_208/"
+    browser.get(link)
+
+    time.sleep(5)
+
+    # Ищем на странице кнопку добавления товара в корзину
+    try:
+        button = WebDriverWait(browser, 5).until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, ".btn-add-to-basket")))
+        found = True
+
+    # Если кнопку не нашли (возникла ошибка), то выполняем этот кусок кода
+    except:
+        found = False
+
+    assert found == True, "Button is not found"
+
+
+if __name__ == '__main__':
+    test_is_button_add_to_basket_present_on_page_coder_at_work()
+
+    test_is_button_add_to_basket_present_on_page_hacking_exposed_wireless()
